5c47930f071004c865157ce7854c85fe7b58df80
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-01-30 21:48:11 +0900

    add

--- scratch/stacking.py
+++ scratch/stacking.py
@@ -105,12 +105,8 @@ va_preds_merged, te_preds_merged = merge_predictions(
 delta, f1_score = bestThresshold(y_va, va_preds_merged)
 print('[validation] best threshold is {:.4f} with F1 score: {:.4f}'.format(delta, f1_score))
 
-y_test = joblib.load('valid_for_emsemble_lable.pkl').values
-print(y_va.shape)
-print(type(y_va))
-print(y_test.shape)
-print(type(y_test))
-delta, f1_score = bestThresshold(y_test, te_preds_merged)
+y_te = joblib.load('valid_for_emsemble_lable.pkl').values
+delta, f1_score = bestThresshold(y_te, te_preds_merged)
 print('[test] best threshold is {:.4f} with F1 score: {:.4f}'.format(delta, f1_score))
 
 df_test = pd.read_csv(os.path.join(INPUT_PATH, "test.csv"))
