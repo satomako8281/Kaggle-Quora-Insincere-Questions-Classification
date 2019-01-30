078420dedeed09f4d3c634a99a7e5128482c77cb
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-01-30 19:32:14 +0900

    add

--- scratch/stacking_hand.py
+++ scratch/stacking_hand.py
@@ -32,18 +32,16 @@ te_preds.append(joblib.load("test_pred_pytorch.pkl")[:, np.newaxis])
 va_preds = np.hstack(va_preds)
 te_preds = np.hstack(te_preds)
 y_va = joblib.load("y_val.pkl")
-print(va_preds.shape)
-print(te_preds.shape)
 
 va_preds_merged = (
-    0.21 * va_preds[0] +
-    0.30 * va_preds[1] +
-    0.11 * va_preds[2] +
-    0.36 * va_preds[3]
+    0.21 * va_preds[:, 0] +
+    0.30 * va_preds[:, 1] +
+    0.11 * va_preds[:, 2] +
+    0.36 * va_preds[:, 3]
 )
 
 print(va_preds_merged.shape)
-print(y_va)
+print(y_va.shape)
 delta, f1_score = bestThresshold(y_va, va_preds_merged)
 print('[Model mean] best threshold is {:.4f} with F1 score: {:.4f}'.format(delta, f1_score))
 
