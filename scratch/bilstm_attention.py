c0868dabd4cb0247a14960594b11e9ed7dd62601
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-01-30 10:13:40 +0900

    add

--- scratch/bilstm_attention.py
+++ scratch/bilstm_attention.py
@@ -386,11 +386,13 @@ paragram_embeddings = load_para(word_index)
 # fasttext_embeddings = load_fasttext(word_index)
 
 # embedding_matrix = np.mean([glove_embeddings, paragram_embeddings, fasttext_embeddings], axis=0)
+
 # embedding_matrix = np.mean([glove_embeddings, paragram_embeddings], axis=0)
 # joblib.dump(embedding_matrix, 'embedding_matrix.pkl', compress=3)
 # del glove_embeddings, paragram_embeddings
 
-embedding_matrix = joblib.load('embeddinng_matrix.pkl')
+embedding_matrix = joblib.load('embedding_matrix.pkl')
+
 # vocab = build_vocab(df['question_text'])
 # add_lower(embedding_matrix, vocab)
 # del glove_embeddings, paragram_embeddings, fasttext_embeddings
