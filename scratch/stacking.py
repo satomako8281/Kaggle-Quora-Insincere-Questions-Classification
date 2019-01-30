817cefb8ea6f77d3ef6e611481a5f5f54eafc8c2
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-01-31 01:07:06 +0900

    add

--- scratch/stacking.py
+++ scratch/stacking.py
@@ -96,7 +96,7 @@ est = xgb.XGBClassifier(
 from sklearn.ensemble import RandomForestClassifier
 est = RandomForestClassifier()
 
-y_te = joblib.load('valid_for_emsemble_lable.pkl').values
+# y_te = joblib.load('valid_for_emsemble_lable.pkl').values
 
 va_preds_merged, te_preds_merged = merge_predictions(
     X_tr=va_preds, y_tr=y_va, X_te=te_preds,
