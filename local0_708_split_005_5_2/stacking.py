5caf5aa9c530415e0feeb3e374f9353d1b623a80
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-02-01 00:43:55 +0900

    add

--- local0_708_split_005_5_2/stacking.py
+++ local0_708_split_005_5_2/stacking.py
@@ -72,8 +72,8 @@ va_pred0 = joblib.load("valid_pred_pytorch_0.pkl")[:, np.newaxis]
 va_pred1 = joblib.load("valid_pred_pytorch_1.pkl")[:, np.newaxis]
 va_pred2 = joblib.load("valid_pred_pytorch_2.pkl")[:, np.newaxis]
 va_preds.append((va_pred0+va_pred1+va_pred2)/3)
-va_preds.append(joblib.load("valid_pred_pytorch_0.pkl")[:, np.newaxis])
-va_preds.append(joblib.load("valid_pred_pytorch_1.pkl")[:, np.newaxis])
+# va_preds.append(joblib.load("valid_pred_pytorch_0.pkl")[:, np.newaxis])
+# va_preds.append(joblib.load("valid_pred_pytorch_1.pkl")[:, np.newaxis])
 te_preds.append(joblib.load("test_pred_tfidf.pkl")[:, np.newaxis])
 te_preds.append(joblib.load("test_pred_mercari.pkl")[:, np.newaxis])
 te_pred0 = joblib.load("test_pred_bilstm_0.pkl")[:, np.newaxis]
