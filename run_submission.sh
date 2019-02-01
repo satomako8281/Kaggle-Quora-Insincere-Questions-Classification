79a6b24fed925c1c13fea4a6dce7fcdafd064527
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-02-01 22:10:45 +0900

    add

--- run_submission.sh
+++ run_submission.sh
@@ -1,6 +1,141 @@
 git fetch && git merge
+echo 0
+python local0_708_split0/tfidf.py
+python local0_708_split0/mercari_mlp_without_embeddings.py
+python local0_708_split0/bilstm_attention.py
+python local0_708_split0/pytorch_modeling.py
+python local0_708_split0/stacking.py
 
 echo 1
+python local0_708_split1/tfidf.py
+python local0_708_split1/mercari_mlp_without_embeddings.py
+python local0_708_split1/bilstm_attention.py
+python local0_708_split1/pytorch_modeling.py
+python local0_708_split1/stacking.py
+
 echo 2
-cat pytorch
-local0_708_split0
+python local0_708_split2/tfidf.py
+python local0_708_split2/mercari_mlp_without_embeddings.py
+python local0_708_split2/bilstm_attention.py
+python local0_708_split2/pytorch_modeling.py
+python local0_708_split2/stacking.py
+
+echo 3
+python local0_708_split3/tfidf.py
+python local0_708_split3/mercari_mlp_without_embeddings.py
+python local0_708_split3/bilstm_attention.py
+python local0_708_split3/pytorch_modeling.py
+python local0_708_split3/stacking.py
+
+echo 4
+python local0_708_split4/tfidf.py
+python local0_708_split4/mercari_mlp_without_embeddings.py
+python local0_708_split4/bilstm_attention.py
+python local0_708_split4/pytorch_modeling.py
+python local0_708_split4/stacking.py
+
+echo 5
+python local0_708_split5/tfidf.py
+python local0_708_split5/mercari_mlp_without_embeddings.py
+python local0_708_split5/bilstm_attention.py
+python local0_708_split5/pytorch_modeling.py
+python local0_708_split5/stacking.py
+
+echo 6
+python local0_708_split6/tfidf.py
+python local0_708_split6/mercari_mlp_without_embeddings.py
+python local0_708_split6/bilstm_attention.py
+python local0_708_split6/pytorch_modeling.py
+python local0_708_split6/stacking.py
+
+echo 7
+python local0_708_split7/tfidf.py
+python local0_708_split7/mercari_mlp_without_embeddings.py
+python local0_708_split7/bilstm_attention.py
+python local0_708_split7/pytorch_modeling.py
+python local0_708_split7/stacking.py
+
+echo 8
+python local0_708_split8/tfidf.py
+python local0_708_split8/mercari_mlp_without_embeddings.py
+python local0_708_split8/bilstm_attention.py
+python local0_708_split8/pytorch_modeling.py
+python local0_708_split8/stacking.py
+
+echo 9
+python local0_708_split9/tfidf.py
+python local0_708_split9/mercari_mlp_without_embeddings.py
+python local0_708_split9/bilstm_attention.py
+python local0_708_split9/pytorch_modeling.py
+python local0_708_split9/stacking.py
+
+echo 10
+python local0_708_split10/tfidf.py
+python local0_708_split10/mercari_mlp_without_embeddings.py
+python local0_708_split10/bilstm_attention.py
+python local0_708_split10/pytorch_modeling.py
+python local0_708_split10/stacking.py
+
+echo 11
+python local0_708_split11/tfidf.py
+python local0_708_split11/mercari_mlp_without_embeddings.py
+python local0_708_split11/bilstm_attention.py
+python local0_708_split11/pytorch_modeling.py
+python local0_708_split11/stacking.py
+
+echo 12
+python local0_708_split12/tfidf.py
+python local0_708_split12/mercari_mlp_without_embeddings.py
+python local0_708_split12/bilstm_attention.py
+python local0_708_split12/pytorch_modeling.py
+python local0_708_split12/stacking.py
+
+echo 13
+python local0_708_split13/tfidf.py
+python local0_708_split13/mercari_mlp_without_embeddings.py
+python local0_708_split13/bilstm_attention.py
+python local0_708_split13/pytorch_modeling.py
+python local0_708_split13/stacking.py
+
+echo 14
+python local0_708_split14/tfidf.py
+python local0_708_split14/mercari_mlp_without_embeddings.py
+python local0_708_split14/bilstm_attention.py
+python local0_708_split14/pytorch_modeling.py
+python local0_708_split14/stacking.py
+
+echo 15
+python local0_708_split15/tfidf.py
+python local0_708_split15/mercari_mlp_without_embeddings.py
+python local0_708_split15/bilstm_attention.py
+python local0_708_split15/pytorch_modeling.py
+python local0_708_split15/stacking.py
+
+echo 16
+python local0_708_split16/tfidf.py
+python local0_708_split16/mercari_mlp_without_embeddings.py
+python local0_708_split16/bilstm_attention.py
+python local0_708_split16/pytorch_modeling.py
+python local0_708_split16/stacking.py
+
+echo 17
+python local0_708_split17/tfidf.py
+python local0_708_split17/mercari_mlp_without_embeddings.py
+python local0_708_split17/bilstm_attention.py
+python local0_708_split17/pytorch_modeling.py
+python local0_708_split17/stacking.py
+
+echo 18
+python local0_708_split18/tfidf.py
+python local0_708_split18/mercari_mlp_without_embeddings.py
+python local0_708_split18/bilstm_attention.py
+python local0_708_split18/pytorch_modeling.py
+python local0_708_split18/stacking.py
+
+echo 19
+python local0_708_split19/tfidf.py
+python local0_708_split19/mercari_mlp_without_embeddings.py
+python local0_708_split19/bilstm_attention.py
+python local0_708_split19/pytorch_modeling.py
+python local0_708_split19/stacking.py
+
