0ec4ed88033bec3e84623be4bdcb074970afc8cc
maida-yasuhiro <a14z7ym@aiit.ac.jp>
2019-02-04 11:02:37 +0900

    add

--- local0_708_split5/test702.py
+++ local0_708_split5/test702.py
@@ -5,25 +5,18 @@ import numpy as np
 import gc
 import re
 import torch
-from torchtext import data
-import spacy
 from tqdm import tqdm_notebook, tnrange
 from tqdm.auto import tqdm
 
 tqdm.pandas(desc='Progress')
 from collections import Counter
-from textblob import TextBlob
-from nltk import word_tokenize
 
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from torch.autograd import Variable
-from torchtext.data import Example
 from sklearn.metrics import f1_score
-import torchtext
 import os
 
 from keras.preprocessing.text import Tokenizer
